61acc3337fadccb1a8ef80eee2a3898edfb9ffb0
William Jones <37749088+wdj82@users.noreply.github.com>
2021-12-08 02:57:24 -0600

    day 8 complete

--- day 08/day08.js
+++ day 08/day08.js
@@ -1,5 +1,106 @@
-import { exampleInput as rawInput } from './rawInput.js';
-// import { puzzleInput as rawInput } from './rawInput.js';
+// import { exampleInput as rawInput } from './rawInput.js';
+import { puzzleInput as rawInput } from './rawInput.js';
 
-console.log('The answer to part one is:', 'dunno');
-console.log('The answer to part two is:', 'dunno');
+// const rawInput = `acedgfb cdfbe gcdfa fbcad dab cefabd cdfgeb eafb cagedb ab | cdfeb fcadb cdfeb cdbaf`;
+
+function partOne() {
+    return rawInput
+        .split('\n')
+        .map((lines) => lines.split('|')[1])
+        .map((output) => output.trim().split(' '))
+        .reduce((acc, curr) => {
+            curr.forEach((value) => {
+                if ((value.length >= 2 && value.length <= 4) || value.length === 7) {
+                    acc += 1;
+                }
+            });
+            return acc;
+        }, 0);
+}
+console.log('The answer to part one is:', partOne());
+
+// returns segments not found in both arrays
+function intersection(array1, array2) {
+    return array1.filter((x) => !array2.includes(x));
+}
+
+function findDigits(input) {
+    // pull out the segments for the unique signals and the outputs, sorting them all
+    const [signals, outputs] = input
+        .split(' | ')
+        .map((segments) => segments.split(' ').map((signal) => signal.split('').sort().join('')));
+
+    const digits = {};
+    const segments = { 2: [], 3: null, 4: null, 5: [], 6: [], 7: null };
+
+    // need these two segments to find 2, 3, and 5
+    let upperLeft = null;
+    let lowerLeft = null;
+
+    // group up the digits by length, saving 3, 4, and 7
+    signals.forEach((signal) => {
+        if (signal.length === 2) {
+            segments[2] = signal.split('');
+            digits[signal] = '1';
+        } else if (signal.length === 3) {
+            segments[3] = signal.split('');
+            digits[signal] = '7';
+        } else if (signal.length === 4) {
+            segments[4] = signal.split('');
+            digits[signal] = '4';
+        } else if (signal.length === 5) {
+            segments[5].push(signal.split(''));
+        } else if (signal.length === 6) {
+            segments[6].push(signal.split(''));
+        } else if (signal.length === 7) {
+            segments[7] = signal.split('');
+            digits[signal] = '8';
+        }
+    });
+
+    // remove the 2 segment from the 4 segment - leaving the upper right and middle segments unknown
+    segments[4] = intersection(segments[4], segments[2]);
+
+    // get the segments missing from 0, 6, and 9 (the six length segments each lack one segment)
+    const missingSegments = segments[6].map((sixes) => [intersection(segments[7], sixes)[0], sixes.join('')]);
+
+    // using the known segments from 1, 4, 7, 8 we can compare them to the missing segments of 0, 6, and 9
+    // 0 lacks the middle segment - locking in it and upper left from 4
+    // 6 lacks the upper right - locking in it and lower right from 1
+    // 9 lacks the lower left - locking in it and bottom from 8 minus the 4 and 7 segments
+    // with 7 locking in the upper segment from 1 all seven segments are known
+    missingSegments.forEach(([segment, digit]) => {
+        if (segments[2].includes(segment)) {
+            digits[digit] = '6';
+        } else if (segments[4].includes(segment)) {
+            digits[digit] = '0';
+            upperLeft = segments[4][0] === segment ? segments[4][1] : segments[4][0];
+        } else if (segments[7].includes(segment)) {
+            digits[digit] = '9';
+            const bottomAndLowerLeft = intersection(segments[7], [...segments[3], ...segments[4]]);
+            lowerLeft = bottomAndLowerLeft[0] === segment ? bottomAndLowerLeft[0] : bottomAndLowerLeft[1];
+        }
+    });
+
+    // knowing the upper left and lower left segments we can get the final digits of 2, 3, and 5 (the five length segments)
+    segments[5].forEach((fives) => {
+        if (fives.includes(upperLeft)) {
+            digits[fives.join('')] = '5';
+        } else if (fives.includes(lowerLeft)) {
+            digits[fives.join('')] = '2';
+        } else {
+            digits[fives.join('')] = '3';
+        }
+    });
+
+    // knowing all the digits now return the four digit output as a number
+    const answer = outputs.map((output) => digits[output]).join('');
+    return Number(answer);
+}
+
+// return the sum of all four digit output values
+function partTwo() {
+    return rawInput.split('\n').reduce((acc, input) => acc + findDigits(input), 0);
+}
+
+console.log('The answer to part two is:', partTwo());
