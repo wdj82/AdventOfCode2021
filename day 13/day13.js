0b2932af45d62767f5027b47ac39ae1f44fc2838
William Jones <37749088+wdj82@users.noreply.github.com>
2021-12-11 11:34:16 -0600

    cleanup for day 11

--- day 13/day13.js
+++ day 13/day13.js
@@ -1,5 +1,5 @@
-// Advent of Code day 12
-// https://adventofcode.com/2021/day/12
+// Advent of Code day 13
+// https://adventofcode.com/2021/day/13
 
 import { exampleInput as rawInput } from './rawInput.js';
 // import { puzzleInput as rawInput } from './rawInput.js';
