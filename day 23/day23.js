a536a9d94b1175d7f645bade2bb76395cf98eaa4
William Jones <37749088+wdj82@users.noreply.github.com>
2021-12-22 17:52:35 -0600

    day 22 complete

---
+++ day 23/day23.js
@@ -0,0 +1,10 @@
+// Advent of Code day 23
+// https://adventofcode.com/2021/day/23
+
+import { exampleInput as rawInput } from './rawInput.js';
+// import { puzzleInput as rawInput } from './rawInput.js';
+
+console.log(`Part one: `, 'dunno');
+console.log(`Part two: `, 'dunno');
+document.getElementById('partOne').appendChild(document.createTextNode('dunno'));
+document.getElementById('partTwo').appendChild(document.createTextNode('dunno'));
